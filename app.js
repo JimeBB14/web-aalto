941c384829eadea014363ada1047934c96d8afea
JimeBB14 <145394078+JimeBB14@users.noreply.github.com>
2024-06-23 23:37:07 +0300

    feedback with buttons

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
-import { getFeedbackCount, incrementFeedbackCount } from "./feedbacks.js";
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import * as feedbackService from "./feedbacks.js";
 
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
 
-app.get("/feedbacks/:value", async (c) => {
-  const value = c.req.param("value");
-  const count = await getFeedbackCount(value);
-  return c.text(`Feedback ${value}: ${count}`);
-});
+const showFeedbackForm = async (c) => {
+  return c.html(await eta.render("index.eta", {}));
+};
 
-app.post("/feedbacks/:value", async (c) => {
-  const value = c.req.param("value");
-  await incrementFeedbackCount(value);
-  const count = await getFeedbackCount(value);
+const addFeedback = async (c) => {
+  const { value } = c.req.param();
+  await feedbackService.addFeedback(value);
+  return c.redirect("/");
+};
+
+const getFeedbackCount = async (c) => {
+  const { value } = c.req.param();
+  const count = await feedbackService.getFeedbackCount(value);
   return c.text(`Feedback ${value}: ${count}`);
-});
+};
+
+app.get("/", (c) => showFeedbackForm(c));
+app.post("/feedbacks/:value", (c) => addFeedback(c));
+app.get("/feedbacks/:value", (c) => getFeedbackCount(c));
 
 export default app;
--- feedbacks.js
+++ feedbacks.js
@@ -1,13 +1,18 @@
-const getFeedbackCount = async (value) => {
-    const kv = await Deno.openKv();
-    const feedback = await kv.get(["feedback", value]);
-    return feedback.value ?? 0;
+const feedbackCounts = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0
   };
   
-  const incrementFeedbackCount = async (value) => {
-    const kv = await Deno.openKv();
-    const currentCount = await getFeedbackCount(value);
-    await kv.set(["feedback", value], currentCount + 1);
+  const addFeedback = async (value) => {
+    feedbackCounts[value]++;
   };
   
-  export { getFeedbackCount, incrementFeedbackCount };
+  const getFeedbackCount = async (value) => {
+    return feedbackCounts[value];
+  };
+  
+  export { addFeedback, getFeedbackCount };
+  
